b32808b289495e987aa23c2565b48b1340cf5210
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2018-09-17 17:27:02 +0200

    Added legacy block for backwards compatibility

---
+++ Legacy/Block/Component.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Creativestyle\ContentConstructorFrontendExtension\Block;
+
+/**
+ * Legacy class only for backwards compatibility with old class name
+ */
+class Component extends \MageSuite\ContentConstructorFrontend\Block\Component {}
