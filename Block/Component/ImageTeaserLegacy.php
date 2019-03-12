23a666e12bd98df9d8319ee740db31fd252d2c22
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2019-03-12 20:04:30 +0100

    New image teaser with content optimizers and text scaling

---
+++ Block/Component/ImageTeaserLegacy.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MageSuite\ContentConstructorFrontend\Block\Component;
+
+class ImageTeaserLegacy extends AbstractComponent
+{
+    protected $_template = 'MageSuite_ContentConstructorFrontend::component/image_teaser_legacy.phtml';
+}
---
+++ Model/Component/ImageTeaserLegacy/Slide.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MageSuite\ContentConstructorFrontend\Model\Component\ImageTeaserLegacy;
+
+class Slide extends \MageSuite\ContentConstructorFrontend\Model\Component\GenericSlide
+{
+
+}
