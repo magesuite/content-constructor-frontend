3b8e8a77acbf0226a1ba8d3060ccc7a1d7152ae5
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2019-03-13 13:38:22 +0100

    Updated length of layout_update_xml field to 16MB

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -21,6 +21,19 @@ class UpgradeSchema implements \Magento\Framework\Setup\UpgradeSchemaInterface
             );
         }
 
+        if (version_compare($context->getVersion(), '1.0.2', '<')) {
+            $setup->getConnection()->changeColumn(
+                $setup->getTable('cms_page'),
+                'layout_update_xml',
+                'layout_update_xml',
+                [
+                    'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                    'length' => '16777216',
+                    'comment' => 'Page Layout Update Content'
+                ]
+            );
+        }
+
         $setup->endSetup();
     }
 }
