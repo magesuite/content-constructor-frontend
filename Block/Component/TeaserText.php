2f1e7a0b36b05dc115fd919efb540d508ac7885f
Marta Siewierska <marta.siewierska-stanczak@creativestyle.pl>
2019-06-10 10:22:33 +0200

    Remove files from teaser-and-text component

--- Block/Component/TeaserText.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace MageSuite\ContentConstructorFrontend\Block\Component;
-
-class TeaserText extends AbstractComponent
-{
-    protected $_template = 'MageSuite_ContentConstructorFrontend::component/teaser_text.phtml';
-}
--- Model/Component/TeaserText.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace MageSuite\ContentConstructorFrontend\Model\Component;
-
-class TeaserText extends \Magento\Framework\DataObject implements ViewModel
-{
-
-}
