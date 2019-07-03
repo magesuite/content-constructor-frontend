830d48826c3dd59afb72332fbafea01f3676210d
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2019-07-03 11:09:09 +0200

    Add missing support for directives in hero carousel

--- Model/Component/HeroCarousel/Slide.php
+++ Model/Component/HeroCarousel/Slide.php
@@ -4,5 +4,13 @@ namespace MageSuite\ContentConstructorFrontend\Model\Component\HeroCarousel;
 
 class Slide extends \MageSuite\ContentConstructorFrontend\Model\Component\GenericSlide
 {
+    public function getHref() {
+        $hero = $this->getData();
 
+        if (isset($hero['href']) and !empty($hero['href'])) {
+            return $this->urlResolver->resolve($hero['href']);
+        }
+
+        return null;
+    }
 }
