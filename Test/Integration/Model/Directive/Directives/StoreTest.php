187e7e6651773de7665557a71878094d15577b60
Adam Karnowka <adam.karnowka@creativestyle.pl>
2019-08-21 14:24:16 +0200

    Support for store directive

---
+++ Test/Integration/Model/Directive/Directives/StoreTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MageSuite\ContentConstructorFrontend\Test\Integration\Model\Directive\Directives\Product;
+
+class StoreTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var \Magento\TestFramework\ObjectManager
+     */
+    protected $objectManager;
+
+    /**
+     * @var \MageSuite\ContentConstructorFrontend\Model\Directive\Directives\Store
+     */
+    protected $directive;
+
+    public function setUp()
+    {
+        $this->objectManager = \Magento\TestFramework\ObjectManager::getInstance();
+
+        $this->directive = $this->objectManager->create(\MageSuite\ContentConstructorFrontend\Model\Directive\Directives\Store::class);
+    }
+
+    /**
+     * @magentoDbIsolation enabled
+     * @magentoDataFixture Magento/Catalog/_files/products.php
+     */
+    public function testItReturnsCorrectUrl()
+    {
+        $this->directive->setArguments(['url' => 'storelocator']);
+
+        $url = $this->directive->getValue();
+
+        $this->assertEquals('http://localhost/index.php/storelocator/', $url);
+    }
+
+    /**
+     * @magentoDbIsolation enabled
+     * @magentoDataFixture Magento/Catalog/_files/products.php
+     */
+    public function testItReturnsBaseUrlWhenUrlArgumentIsNotSet()
+    {
+        $url = $this->directive->getValue();
+
+        $this->assertEquals('http://localhost/index.php/', $url);
+    }
+}
