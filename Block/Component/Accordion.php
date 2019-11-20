872dcb122d197edbcb68ec442302fc72acc088f5
Blazej Doleska <blazej.doleska@creativestyle.pl>
2019-11-20 14:28:29 +0100

    base accordion component configuration

---
+++ Block/Component/Accordion.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MageSuite\ContentConstructorFrontend\Block\Component;
+
+class Accordion extends AbstractComponent
+{
+    protected $_template = 'MageSuite_ContentConstructorFrontend::component/accordion.phtml';
+}
