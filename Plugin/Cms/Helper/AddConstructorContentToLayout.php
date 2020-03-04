94a0af7cfd447d5be44d93435904314c60036670
Paweł Złamaniec <pawel.zlamaniec@creativestyle.pl>
2020-03-04 09:32:33 +0100

    Add plugins to display constructor content data on frontend pages

---
+++ Plugin/Cms/Helper/AddConstructorContentToLayout.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MageSuite\ContentConstructorFrontend\Plugin\Cms\Helper;
+
+class AddConstructorContentToLayout
+{
+    /**
+     * @var \Magento\Cms\Model\Page
+     */
+    protected $page;
+
+    /**
+     * @var \MageSuite\ContentConstructorFrontend\Service\LayoutContentUpdateService
+     */
+    protected $layoutContentUpdateService;
+
+    public function __construct(
+        \Magento\Cms\Model\Page $page,
+        \MageSuite\ContentConstructorFrontend\Service\LayoutContentUpdateService $layoutContentUpdateService
+    ) {
+        $this->page = $page;
+        $this->layoutContentUpdateService = $layoutContentUpdateService;
+    }
+
+    public function afterPrepareResultPage(\Magento\Cms\Helper\Page $subject, \Magento\Framework\View\Result\Page $resultPage)
+    {
+        $this->layoutContentUpdateService->addContentConstructorToUpdateLayout($resultPage, $this->page);
+    }
+}
