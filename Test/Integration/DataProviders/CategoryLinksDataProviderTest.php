f5cf74ba631f40a4d5daa7862a912f528abac2a3
Adam Karnowka <adam.karnowka@creativestyle.pl>
2020-08-19 09:02:17 +0200

    Dont render disabled categories

--- Test/Integration/DataProviders/CategoryLinksDataProviderTest.php
+++ Test/Integration/DataProviders/CategoryLinksDataProviderTest.php
@@ -5,30 +5,34 @@ namespace MageSuite\ContentConstructorFrontend\Test\Integration\DataProviders;
 class CategoryLinksDataProviderTest extends \PHPUnit\Framework\TestCase
 {
     const MAIN_CATEGORY_ID = 333;
-    const SUBCATEGORIES_IDS = [334,335,336];
+    const SUBCATEGORIES_IDS = [334,335,336,338];
     const ORDERED_SUBCATEGORIES_IDS = [335,334,336];
 
     protected $expectedCategoriesStructure = [
         'main_category' => [
             'name' => 'Main category',
             'url' => 'http://localhost/index.php/main-category.html',
-            'products_count' => 2
+            'products_count' => 2,
+            'is_active' => 1
         ],
         'sub_categories' => [
             [
                 'name' => 'First subcategory',
                 'url' => 'http://localhost/index.php/main-category/first-subcategory.html',
-                'products_count' => 1
+                'products_count' => 1,
+                'is_active' => 1
             ],
             [
                 'name' => 'Second subcategory',
                 'url' => 'http://localhost/index.php/main-category/second-subcategory.html',
-                'products_count' => 0
+                'products_count' => 0,
+                'is_active' => 1
             ],
             [
                 'name' => 'Third subcategory',
                 'url' => 'http://localhost/index.php/main-category/third-subcategory.html',
-                'products_count' => 1
+                'products_count' => 1,
+                'is_active' => 1
             ]
         ]
     ];
@@ -58,7 +62,6 @@ class CategoryLinksDataProviderTest extends \PHPUnit\Framework\TestCase
      */
     public function testItReturnsCorrectCategoriesStructure() {
         $result = $this->dataProvider->getCategories(self::MAIN_CATEGORY_ID, self::SUBCATEGORIES_IDS);
-
         $this->assertEquals($this->expectedCategoriesStructure, $result);
     }
 
